e979b64dc8ac9569bee45887dbfee3fa907d6bcb
zhouzhiqiang1 <zhou83955@sina.com>
2016-03-09 20:59:07 +0800

    开始

---
+++ scholars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scholars
+//
+//  Created by mgc1105 on 16/3/9.
+//  Copyright © 2016年 Mac Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scholars/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  scholars
+//
+//  Created by mgc1105 on 16/3/9.
+//  Copyright © 2016年 Mac Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
